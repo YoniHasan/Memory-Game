0a8e73430a2fe7d6ef37f676814235e7f9fc3448
Yehonatan Hasan <yonh03@gmail.com>
2022-04-05 19:42:10 +0300

    add class shuffle

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ const cards = ["A","B","C","A","B","C"];
 function AddElemnt(v,i,arr){
 const element = document.createElement("div")
 element.innerHTML= v
+element.id=i
+element.className="card"
 const board = document.getElementById("board")
 board.appendChild(element)
 }
+shuffleCards(cards)
 cards.filter(AddElemnt);
+//פונקציה שמערבבת את המערך ואז שם בhtml 
+
+function shuffleCards(cards)
+{
+cards= cards.sort (()=>Math.random() - 0.5)
+}
